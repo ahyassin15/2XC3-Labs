8d1bb7b0211520de19e0c18ff2f3e7adb766e259
Ahmed <ah.yassin05@gmail.com>
2024-10-03 17:28:45 -0400

    Created functions for usage instructions and help info

--- quiz.c
+++ quiz.c
@@ -3,6 +3,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+//Function to display usage instructions for the quiz program to the user
+void display_usage() {
+    puts("Usage: quiz [-#] [<answer>]");
+    puts("Try 'quiz --help' for more information.\n");
+}
+
+//Function to display help information to the user, explaining how to use the utility
+void display_help() {
+    puts("This quiz program will test your knowledge on Linux, C, and Git");
+    puts("Usage:");
+    puts("  -1            Display question 1 (Linux)");
+    puts("  -2            Display question 2 (C)");
+    puts("  -3            Display question 3 (Git)");
+    puts("  -# <answer>   Provide your answer for question #");
+    puts("  --help        Show help message\n");
+}
+
 //Main function for program execution
 int main(int argc, char*argv[]) {
     
