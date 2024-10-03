2518e776fd06e9a7cd9716a1f72b80fae6a9c957
Ahmed <ah.yassin05@gmail.com>
2024-10-03 17:26:34 -0400

    Initialized strings for questions and answers, created makefile

---
+++ quiz.c
@@ -0,0 +1,19 @@
+//Include necessary libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Main function for program execution
+int main(int argc, char*argv[]) {
+    
+    //Declare and initialize strings for each question and their corresponding answer accordingly
+    char *question1 = "What is the command to move into a directory in Linux?";
+    char *answer1 = "In C, what does the 'printf' function do?";
+    
+    char *question2 = "cd";
+    char *answer2 = "Prints output";
+    
+    char *question3 = "What is the command to initialize a new Git repository?";
+    char *answer3 = "git init";
+
+}
